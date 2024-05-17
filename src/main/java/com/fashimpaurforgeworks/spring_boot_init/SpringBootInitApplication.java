d504d6488e94dcc8a3650fa2ef694238df0c02ab
Dennis Fashimpaur <dfashimpaur@gmail.com>
2024-05-17 16:24:06 -0500

    Attempted DNS resolver error fix at runtime

--- src/main/java/com/fashimpaurforgeworks/spring_boot_init/SpringBootInitApplication.java
+++ src/main/java/com/fashimpaurforgeworks/spring_boot_init/SpringBootInitApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootInitApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootInitApplication.class, args);
+
     }
 
 }
